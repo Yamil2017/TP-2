8c1ebbc3c7195625c0033b0bc10e8d1762236333
Yamil2017 <pechyar@hotmail.com>
2017-04-07 19:06:35 -0300

    Add files via upload

---
+++ TP2.cpp
@@ -0,0 +1,30 @@
+//============================================================================
+// Name        : TP2.cpp
+// Author      : Yamil
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a, b;
+
+	cout << "Ingrese número " << endl; // Inngreso de valor a
+	cin>>a;
+
+
+	cout << "Ingrese número " << endl; // Inngreso de valor b
+	cin>>b;
+
+	if (a<b){
+		cout << "b es mayor que a \n" << endl;}
+		else if (a>b){
+			cout << "a es mayor que b \n" << endl;
+		}
+		else
+			cout << "a es igual que b \n" << endl;
+			}
+
